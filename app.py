5fc45bd90561c475184ab71b658bb393b1297c0b
nrenteria <59494605+nrenteria@users.noreply.github.com>
2020-05-04 17:13:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask, render_template, url_for, flash, request, redirect
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 
+from celery import Celery
 
 app = Flask(__name__)
 
