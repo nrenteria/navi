35eadbe098e0636138813bdff5f399eb2ce48874
gehanjay <60203681+gehanjay@users.noreply.github.com>
2020-05-04 16:59:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,47 +3,14 @@ from flask import Flask, render_template, url_for, flash, request, redirect
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
 
-UPLOAD_FOLDER = 'C:/Users/Gehan/Documents/UT/Spring 2020/me 369/flask_app/Uploads'
-ALLOWED_EXTENSIONS = { 'pdf', 'ics'}
 
 app = Flask(__name__)
 
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
     return render_template('sms2.html')
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-
-
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
