0c35e85137b7649f8ba5235496771b6bfc8e062a
gehanjay <60203681+gehanjay@users.noreply.github.com>
2020-05-04 11:41:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, render_template, url_for, flash, request, redirect
 from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
-UPLOAD_FOLDER = '/UPLOAD_FOLDER'
+UPLOAD_FOLDER = 'C:/Users/Gehan/Documents/UT/Spring 2020/me 369/flask_app/Uploads'
 ALLOWED_EXTENSIONS = { 'pdf', 'ics'}
 
 app = Flask(__name__)
@@ -34,6 +36,12 @@ def index():
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     return render_template('sms2.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 
