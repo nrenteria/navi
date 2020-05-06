bc668f67b95a730418d0a337a5f6060cf603f87b
nrenteria <59494605+nrenteria@users.noreply.github.com>
2020-05-06 13:07:44 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, url_for, flash, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from task import send_message
+from tasks import send_message, parse_datetime
+from celery import Celery
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+@celery.task
+def schedule_message(phoneNumber, message):
+    send_message(phoneNumber, message)
+    return
 
 @app.route('/', methods=['GET'])
 def index():
@@ -11,14 +22,16 @@ def index():
 @app.route("/done", methods=['POST'])
 def done():
     phoneNumber = request.form['phone']
-    reminderTime = request.form['remindertime']
-    zoomLink = request.form['url']
+    reminderTimeInput = request.form['remindertime']
+    message = request.form['message']
 
-    if send_message(phoneNumber, zoomLink):
-        flash("Text has been sent!")
+    if len(phoneNumber) == 10 and int(phoneNumber):
+        reminderTime = parse_datetime(reminderTimeInput)
+        schedule_message.apply_async(args=[phoneNumber, message], eta = reminderTime)
+        flash("Reminder is scheduled!")
         return redirect(url_for('index'))
     else:
-        flash("Text message was unable to send. Try again.")
+        flash("Unable to schedule reminder. Please try again.")
         return redirect(url_for('index'))
 
 if __name__ == "__main__":
